2707bee855f94ab59dfd2eafaad869c389d7c43e
collotek <collinskipkorir94@gmail.com>
2023-05-16 10:11:11 +0300

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -69,18 +69,3 @@ class BaseModel:
         our_dict["created_at"] = self.created_at.isoformat()
         our_dict["updated_at"] = self.updated_at.isoformat()
         return our_dict
-
-
-
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
